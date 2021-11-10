8a23214502de758dd573e58b66649ab5a758618b
shforeal <rivena14798@gmail.com>
2021-11-10 11:27:09 +0200

    change files

---
+++ js/apps/mail/services/emails-db.js
@@ -0,0 +1,44 @@
+export const emailsDB = {
+   emailsData,
+};
+
+function emailsData() {
+  return [
+    {
+      id: 'qwer',
+      subject:
+        "What's up with you? Here's how to find out from yolo",
+      body: "The opening keynote of water US this year was You , giving his State of the address. He walked us through the journey of getting  from a prototype to a reality the past year. He also dove into Vue's overall growth in the community.",
+      isRead: false,
+      sentAt: new Date(),
+      to: 'user@appsus.com'
+    },
+    {
+      id: 'abcd',
+      subject:
+        "What's up with you? Here's how to find out from yolo",
+      body: "The opening keynote of water US this year was You , giving his State of the address. He walked us through the journey of getting  from a prototype to a reality the past year. He also dove into Vue's overall growth in the community.",
+      isRead: false,
+      sentAt: new Date(),
+      to: 'user@appsus.com'
+    },
+    {
+      id: 'jkhy',
+      subject:
+        "What's up with you? Here's how to find out from yolo",
+      body: "The opening keynote of water US this year was You , giving his State of the address. He walked us through the journey of getting  from a prototype to a reality the past year. He also dove into Vue's overall growth in the community.",
+      isRead: false,
+      sentAt: new Date(),
+      to: 'user@appsus.com'
+    },
+    {
+      id: 'lopr',
+      subject:
+        "What's up with you? Here's how to find out from yolo",
+      body: "The opening keynote of water US this year was You , giving his State of the address. He walked us through the journey of getting  from a prototype to a reality the past year. He also dove into Vue's overall growth in the community.",
+      isRead: false,
+      sentAt: new Date(),
+      to: 'user@appsus.com'
+    },
+  ];
+}
