0e8ff3c6e8f668023a3370922e461c7cd60907f9
rotem <rotembub@gmail.com>
2021-11-10 10:41:20 +0200

    testing

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -7,7 +7,7 @@ export default {
         <nav>
             <router-link to="/">Home</router-link> |
             <router-link to="/keep">Keep</router-link> |
-            <router-link to="/mail">Mail</router-link> |
+            <router-link to="/mail">Mail!</router-link> |
             <router-link to="/book">Books</router-link> |
             <!-- <router-link to="/about">About</router-link> -->
         </nav>
