bc8340f60f35742944a4a5c020f95041bd19f301
rotem <rotembub@gmail.com>
2021-11-11 14:01:40 +0200

    minor css changes

--- js/apps/keep/cmps/note-preview.cmp.js
+++ js/apps/keep/cmps/note-preview.cmp.js
@@ -17,7 +17,7 @@ export default {
                 <button @click.stop.prevent="copyNote">Copy</button> 
                 <button @click.stop.prevent="toggleColors">🎨</button>
                 <div v-if="colorOpen" class="color-options">
-                    <span @click.stop.prevent="setColor(color)" class="color-span" v-for="color in colors" :style="{ 'background-color': color }">C</span>
+                    <span @click.stop.prevent="setColor(color)" class="color-span" v-for="color in colors" :style="{ 'background-color': color }">Co</span>
                 </div>
                 
 
