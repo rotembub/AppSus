aaf1f4c664ca242fa4016fafbebf3baa3b7f24c7
shforeal <rivena14798@gmail.com>
2021-11-12 18:29:52 +0200

    style

--- js/apps/mail/cmps/email-preview.cmp.js
+++ js/apps/mail/cmps/email-preview.cmp.js
@@ -18,12 +18,12 @@ export default {
         </div>
       
      </td>
-    <!-- <div class="email-icons"> -->
+    
       <td><span @click.stop="onToggleRead(email)"><i  class="far fa-envelope" v-if="!email.isRead"></i><i class="far fa-envelope-open" v-if="email.isRead"></i></span></td>  
       <td><span @click.stop="onToggleStar(email)" :class="{star: isActive}"><i class="fas fa-star"></i></span></td>  
       <td><span @click.stop="onRemove(email.id)"><i class="fas fa-trash"></i></span></td> 
       <td><router-link :to="'/note/'+email.id"><i class="far fa-clipboard"></i></router-link></td>
-    <!-- </div> -->
+    
    </div> 
                  
     `,
